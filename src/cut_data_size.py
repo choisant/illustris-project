d6747de3f673bfb840e6f64ae91d8d26f44a57e2
choisant <aurora.grefsrud@gmail.com>
2020-08-26 10:20:42 +0200

    Better documentation

--- src/cut_data_size.py
+++ src/cut_data_size.py
@@ -2,6 +2,11 @@ import illustris_python as il
 import pandas as pd
 import numpy as np
 
+"""
+This script takes in the Group catalog data from the .hdf5 files, and uses the pandasformat.py file to convert the data to the pandas DataFrame format.
+The data is then run through the desired filter, and saved as a .csv file in the cutdata folder of the relevant simulation.
+"""
+#read in data
 basePath = "./data/tng100-1/output"
 subhaloFields = ["SubhaloMass", 'SubhaloMassType', 'SubhaloFlag', "SubhaloLen"]
 haloFields = ["GroupMass", "GroupMassType", "GroupNsubs", "GroupFirstSub"]
@@ -11,9 +16,7 @@ halos = il.groupcat.loadHalos(basePath,99,fields=haloFields)
 dfSubhalos = il.pandasformat.dictToPandas(subhalos)
 dfHalos = il.pandasformat.dictToPandas(halos)
 
-#Condition to drop data points
-minStellarMass = 0.1 #*10**10 solar masses
-
+#filters
 def subhaloFlagDrop (df):
     indexNames = df[df["SubhaloFlag"] == 0].index
     df.drop(indexNames, inplace = True)
@@ -29,20 +32,15 @@ def minParticles (df, minPart):
     df.drop(indexNames, inplace = True)
     return df
 
-def minYMass(df, x, Y, haloType):
+def minYMass(df, minMass, Y, haloType):
     particleType = (haloType+"Mass"+Y)
     if haloType == "Subhalo":
         df = subhaloFlagDrop(df)
         df = darkMatterZeros(df)
     #Get indices that fail to meet this condition.
-    indexNames = df[df[particleType] < x].index
+    indexNames = df[df[particleType] < minMass].index
     df.drop(indexNames, inplace = True)
     return df
-
-def saveDataCSV(df, haloType, filename, tngFolder):
-    path = "./data/"+tngFolder+"/cutdata/"+haloType+"_"+filename+".csv"
-    f = open(path, "a+") #Create file if it does not already exist.
-    newdf.to_csv(path)
 
 def centralGalaxies(dfHalos, dfSubhalos):
     #dfHalos must have the key-value pair GroupFirstSub
@@ -50,11 +48,15 @@ def centralGalaxies(dfHalos, dfSubhalos):
     centralIndices = centralIndices[centralIndices > 0]
     centrals = dfSubhalos.iloc[centralIndices]
     return centrals
+
+#Saving the data in the correct format
+def saveDataCSV(df, haloType, filename, tngFolder):
+    path = "./data/"+tngFolder+"/cutdata/"+haloType+"_"+filename+".csv"
+    f = open(path, "a+") #Create file if it does not already exist.
+    newdf.to_csv(path)
     
 #newdf = minYMass(dfSubhalos, minStellarMass, Y = "DM", haloType = "Subhalo")
 newdf = centralGalaxies(dfHalos, dfSubhalos)
-newdf2 = minYMass(newdf, minStellarMass, Y = "Stellar", haloType = "Subhalo")
+newdf2 = minYMass(newdf, minMass = 0.1, Y = "Stellar", haloType = "Subhalo")
 
-saveDataCSV(newdf2, haloType="Subhalo", filename = "Centrals_minE9_SM_SMHR", tngFolder = "tng100-1")
-#saveDataCSV(newdf, haloType="Subhalo", filename = "minE9_DM", tngFolder = "tng100-1")
-
+#saveDataCSV(newdf2, haloType="Subhalo", filename = "Centrals_minE9_SM_SMHR", tngFolder = "tng100-1")
