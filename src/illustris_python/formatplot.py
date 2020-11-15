8f18908ef5db6aadc64fd43d46da6625bb711972
choisant <aurora.grefsrud@gmail.com>
2020-11-15 14:37:20 +0100

    Changed the figures a lot

--- src/illustris_python/formatplot.py
+++ src/illustris_python/formatplot.py
@@ -2,8 +2,7 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import pandas as pd
 import numpy as np
-from matplotlib.ticker import ScalarFormatter
-
+from matplotlib.ticker import ScalarFormatter, MultipleLocator, FormatStrFormatter, AutoMinorLocator
 TEXTSIZE = 16
 
 def log_formater(df):
@@ -13,134 +12,125 @@ def log_formater(df):
             df_log[key] = np.log10(list(df[key]))
     return df_log
 
-def VD_SM(title, ax, x0=1.5, x1=3, y0=9, y1=12):
+def VD_SM(ax, x0=1.5, x1=3, y0=9, y1=12):
     ax.set(xlim=(x0, x1), ylim=(y0, y1))
-    #ax.set_title(title)
     ax.set_xlabel(r'log($\sigma$ [km/s])', fontsize=TEXTSIZE)
-    ax.set_ylabel(r"log($M_*$ [$ M_\odot $])", fontsize=TEXTSIZE)
-    ax.grid(True, which="both", linestyle='--')
-    ax.legend(loc = 4)
+    ax.set_ylabel(r"$log(M_{*})$ [$ M_\odot $]", fontsize=TEXTSIZE)
+    ax.tick_params(which='both', direction='in', top=True, right=True, labelsize=TEXTSIZE*0.8)
+    ax.minorticks_on()
+    ax.legend(loc=4)
 
-def VSigma_SM(title, ax, x0=0, x1=2.5, y0=10**9, y1=10**12):
+def VSigma_SM(ax, x0=0, x1=2.5, y0=10**9, y1=10**12):
     ax.set(xlim=(x0, x1), ylim=(y0, y1))
-    #ax.set_title(title)
     ax.set_xlabel(r'$V_{max}/\sigma$', fontsize=TEXTSIZE)
     ax.set_ylabel(r"$M_*$ [$ M_\odot $]", fontsize=TEXTSIZE)
-    ax.tick_params(labelsize=TEXTSIZE)
-    ax.grid(True, which="both", linestyle='-')
+    ax.tick_params(which='both', direction='in', top=True, right=True, labelsize=TEXTSIZE*0.8)
+    ax.minorticks_on()
     ax.legend()
 
-def Vmax_SM(title, ax, x0=1.5, x1=2.5, y0=9, y1=12):
+def Vmax_SM(ax, x0=1.5, x1=2.5, y0=9, y1=11.5):
     ax.set(xlim=(x0, x1), ylim=(y0, y1))
-    #ax.set_title(title)
-    ax.set_xlabel(r'log ($V_{max}$ [km/s])', fontsize=TEXTSIZE)
-    ax.set_ylabel(r"log ($M_*$ [$ M_\odot $])", fontsize=TEXTSIZE)
-    ax.tick_params(labelsize=TEXTSIZE)
-    ax.grid(True, which="both", linestyle='-')
+    ax.set_xlabel(r'$log(V_{max})$ [km/s]', fontsize=TEXTSIZE)
+    ax.set_ylabel(r"$log(M_*)$ [$ M_\odot $]", fontsize=TEXTSIZE)
+    ax.tick_params(which='both', direction='in', top=True, right=True, labelsize=TEXTSIZE*0.8)
+    ax.minorticks_on()
     ax.legend()
 
-def CV_SM(title, ax, x0=1.5, x1=2.5, y0=9, y1=12):
+def CV_SM(ax, x0=1.5, x1=2.5, y0=9, y1=12):
     plt.axis([x0, x1, y0, y1])
-    plt.title(title)
-    plt.xlabel(r'log($V_{circ}$ [km/s])', fontsize=TEXTSIZE)
-    plt.ylabel(r"log($M_*$ [$ M_\odot $])", fontsize=TEXTSIZE)
-    ax.tick_params(labelsize=TEXTSIZE)
+    plt.xlabel(r'$log(V_{circ})$ [km/s]', fontsize=TEXTSIZE)
+    plt.ylabel(r"$log(M_{*})$ [$ M_\odot $]", fontsize=TEXTSIZE)
+    ax.tick_params(which='both', direction='in', top=True, right=True, labelsize=TEXTSIZE*0.8)
+    ax.minorticks_on()
     plt.legend()
 
 def C_SM(color, ax, x0=9, x1=12, y0=-1, y1=1):
     ax.set(xlim=(x0, x1), ylim=(y0, y1))
     ax.set_ylabel(color + " [mag]", fontsize=TEXTSIZE)
-    ax.set_xlabel(r"$M_*$ [$ M_\odot $]", fontsize=TEXTSIZE)
-    ax.tick_params(labelsize=TEXTSIZE)
+    ax.set_xlabel(r"$log(M_*)$ [$ M_\odot $]", fontsize=TEXTSIZE)
+    ax.tick_params(which='both', direction='in', top=True, right=True, labelsize=TEXTSIZE*0.8)
+    ax.minorticks_on()
     ax.legend(loc=4)
     
 def PDF_C(color, ax):
     ax.set_ylabel("PDF", fontsize=TEXTSIZE)
     ax.set_xlabel(color + " color [mag]", fontsize=TEXTSIZE)
-    ax.tick_params(labelsize=TEXTSIZE)
+    ax.tick_params(which='both', direction='in', top=True, right=True, labelsize=TEXTSIZE*0.8)
+    ax.minorticks_on()
     ax.legend()
 
-def HM_SM(title, ax, x0=11, x1=14, y0=(9.0), y1=(12)):
+def HM_SM(ax, text="", x0=11, x1=14, y0=(9.0), y1=(12)):
     ax.set(xlim=(x0, x1), ylim=(y0, y1))
-    #ax.set_title(title)
-    ax.set_xlabel(r'log($M_{halo}$ [$ M_\odot $])', fontsize=TEXTSIZE)
-    ax.set_ylabel(r'log($M_{*}$ [$ M_\odot $])', fontsize=TEXTSIZE)
-    ax.tick_params(labelsize=TEXTSIZE)
-    ax.grid(True, which="both", linestyle='-')
-    ax.legend()
+    ax.set_xlabel(r'$log(M_{halo})$ [$ M_\odot $]', fontsize=TEXTSIZE)
+    ax.set_ylabel(r'$log(M_{*})$ [$ M_\odot $]', fontsize=TEXTSIZE)
+    ax.tick_params(which='both', direction='in', top=True, right=True, labelsize=TEXTSIZE*0.8)
+    ax.minorticks_on()
+    ax.text(0.05, 0.92, text, fontsize=TEXTSIZE, fontweight='bold', transform=ax.transAxes)
+    ax.legend(loc=4)
 
-def R_SM(title, ax, x0=(-1), x1=2, y0=9, y1=12):
+def R_SM(ax, x0=(-1), x1=2, y0=9, y1=12):
     ax.set(xlim=(x0, x1), ylim=(y0, y1))
-    #ax.set_title(title)
-    ax.set_xlabel(r'log($r_e$ [kpc])', fontsize=TEXTSIZE)
-    ax.set_ylabel(r'log($M_*$ [$ M_\odot $])', fontsize=TEXTSIZE)
-    ax.tick_params(labelsize=TEXTSIZE)
-    ax.grid(True, which="both", linestyle='-')
+    ax.set_xlabel(r'$log(r_e)$ [kpc]', fontsize=TEXTSIZE)
+    ax.set_ylabel(r'$log(M_{*})$ [$ M_\odot $]', fontsize=TEXTSIZE)
+    ax.tick_params(which='both', direction='in', top=True, right=True, labelsize=TEXTSIZE*0.8)
+    ax.minorticks_on()
     ax.legend(loc=4)
 
 
-def SM_R(title, ax, x0=(9), x1=13, y0=(-1), y1=2):
+def SM_R(ax, x0=(9), x1=13, y0=(-1), y1=2):
     ax.set(xlim=(x0, x1), ylim=(y0, y1))
-    #ax.set_title(title)
-    ax.set_ylabel(r'log($r_e$ [kpc])', fontsize=TEXTSIZE)
-    ax.set_xlabel(r"log($M_*$ [$ M_\odot $])", fontsize=TEXTSIZE)
-    ax.tick_params(labelsize=TEXTSIZE)
-    ax.grid(True, which="both", linestyle='-')
+    ax.set_ylabel(r'$log(r_e)$ [kpc]', fontsize=TEXTSIZE)
+    ax.set_xlabel(r"$log(M_{*})$ [$ M_\odot $]", fontsize=TEXTSIZE)
+    ax.tick_params(which='both', direction='in', top=True, right=True, labelsize=TEXTSIZE*0.8)
+    ax.minorticks_on()
     ax.legend()
 
-def R_VD(title, ax, x0=(-1), x1=2, y0=1.5, y1=3):
+def R_VD(ax, x0=(-1), x1=2, y0=1.5, y1=3):
     ax.set(xlim=(x0, x1), ylim=(y0, y1))
-
-    #ax.set_title(title)
-    ax.set_xlabel(r'log($r_e$ [kpc/h])', fontsize=TEXTSIZE)
-    ax.set_ylabel(r'log($\sigma$ [km/s])', fontsize=TEXTSIZE)
-    ax.tick_params(labelsize=TEXTSIZE)
-    ax.grid(True, which="both", linestyle='-')
+    ax.set_xlabel(r'$log(r_e)$ [kpc/h]', fontsize=TEXTSIZE)
+    ax.set_ylabel(r'$log(\sigma)$ [km/s]', fontsize=TEXTSIZE)
+    ax.tick_params(which='both', direction='in', top=True, right=True, labelsize=TEXTSIZE*0.8)
+    ax.minorticks_on()
     ax.legend(loc=4)
 
-def SM_SFR(title, df, x0=9, x1=13, y0=(-4), y1=0):
+def SM_SFR(ax, x0=9, x1=13, y0=(-4), y1=0):
     plt.axis([x0, x1, y0, y1])
-    #plt.title(title + ", N = " + str(len(df["SubhaloMassDM"])))
-    plt.xlabel(r'log($M_*$ [$ M_\odot $])', fontsize=TEXTSIZE)
-    plt.ylabel(r"log(sSFR [$ Gyr^{-1} $])", fontsize=TEXTSIZE)
-    ax.tick_params(labelsize=TEXTSIZE)
-    ax.grid(True, which="both", linestyle='-')
+    plt.xlabel(r'$log(M_{*})$ [$ M_\odot $]', fontsize=TEXTSIZE)
+    plt.ylabel(r"$log(sSFR)$ [$ Gyr^{-1} $]", fontsize=TEXTSIZE)
+    ax.tick_params(which='both', direction='in', top=True, right=True, labelsize=TEXTSIZE*0.8)
+    ax.minorticks_on()
     plt.legend()
 
-def VD_BH(title, df, ax, x0=1, x1=3, y0=(6), y1=(10)):
+def VD_BH(ax, x0=1, x1=3, y0=(6), y1=(10)):
     ax.set(xlim=(x0, x1), ylim=(y0, y1))
-    ax.set_ylabel(r"log($M_{BH}$ [$ M_\odot $])", fontsize=TEXTSIZE)
-    ax.set_xlabel(r'log($\sigma$ [km/s])', fontsize=TEXTSIZE)
-    ax.tick_params(labelsize=TEXTSIZE)
-    #ax.set_title(title + ", N = " + str(len(df["SubhaloMassDM"])))
-    ax.grid(True, which="both", linestyle='-')
+    ax.set_ylabel(r"$log(M_{BH})$ [$ M_\odot $]", fontsize=TEXTSIZE)
+    ax.set_xlabel(r'$log($\sigma)$ [km/s]', fontsize=TEXTSIZE)
+    ax.tick_params(which='both', direction='in', top=True, right=True, labelsize=TEXTSIZE*0.8)
+    ax.minorticks_on()
     ax.legend()
 
-def SM_BH(title, df, ax, x0=9, x1=(13), y0=(6), y1=(10)):
+def SM_BH(ax, x0=9, x1=(13), y0=(6), y1=(10)):
     ax.set(xlim=(x0, x1), ylim=(y0, y1))
-    ax.set_ylabel(r"log($M_{BH}$ [$ M_\odot $])", fontsize=TEXTSIZE)
-    ax.set_xlabel(r"log($M_*$ [$ M_\odot $])", fontsize=TEXTSIZE)
-    ax.tick_params(labelsize=TEXTSIZE)
-    #ax.set_title(title + ", N = " + str(len(df["SubhaloMassDM"])))
-    ax.grid(True, which="both", linestyle='-')
+    ax.set_ylabel(r"$log(M_{BH})$ [$ M_\odot $]", fontsize=TEXTSIZE)
+    ax.set_xlabel(r"$log(M_{*})$ [$ M_\odot $]", fontsize=TEXTSIZE)
+    ax.tick_params(which='both', direction='in', top=True, right=True, labelsize=TEXTSIZE*0.8)
+    ax.minorticks_on()
     ax.legend()
 
-def DM_BH(title, df, ax, x0=10, x1=(14), y0=(6), y1=(10)):
+def DM_BH(ax, x0=10, x1=(14), y0=(6), y1=(10)):
     ax.set(xlim=(x0, x1), ylim=(y0, y1))
-    ax.set_ylabel(r"log($M_{BH}$ [$ M_\odot $])", fontsize=TEXTSIZE)
-    ax.set_xlabel(r"log($M_{DM}$ [$ M_\odot $])", fontsize=TEXTSIZE)
-    ax.tick_params(labelsize=TEXTSIZE)
-    #ax.set_title(title + ", N = " + str(len(df["SubhaloMassDM"])))
-    ax.grid(True, which="both", linestyle='-')
+    ax.set_ylabel(r"$log(M_{BH})$ [$ M_\odot $]", fontsize=TEXTSIZE)
+    ax.set_xlabel(r"$log(M_{DM})$ [$ M_\odot $]", fontsize=TEXTSIZE)
+    ax.tick_params(which='both', direction='in', top=True, right=True, labelsize=TEXTSIZE*0.8)
+    ax.minorticks_on()
     ax.legend()
 
-def R_BH(title, df, ax, x0=0, x1=2, y0=(6), y1=(10)):
+def R_BH(ax, x0=0, x1=2, y0=(6), y1=(10)):
     ax.set(xlim=(x0, x1), ylim=(y0, y1))
-    ax.set_ylabel(r"log($M_{BH}$ [$ M_\odot $])", fontsize=TEXTSIZE)
-    ax.set_xlabel(r'log($r_e$ [ckpc/h])', fontsize=TEXTSIZE)
-    ax.tick_params(labelsize=TEXTSIZE)
-    #ax.set_title(title + ", N = " + str(len(df["SubhaloMassDM"])))
-    ax.grid(True, which="both", linestyle='-')
+    ax.set_ylabel(r"$log(M_{BH})$ [$ M_\odot $]", fontsize=TEXTSIZE)
+    ax.set_xlabel(r'$log(r_e)$ [ckpc/h]', fontsize=TEXTSIZE)
+    ax.tick_params(which='both', direction='in', top=True, right=True, labelsize=TEXTSIZE*0.8)
+    ax.minorticks_on()
     ax.legend()
 
 def FP_3D(df):
